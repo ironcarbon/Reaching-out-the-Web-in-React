04700cf06ec452fe64024a4022ecafab565a447b
ipek <ipekcivicakan@gmail.com>
2018-11-10 13:29:54 -0800

    handling errors locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,6 +9,7 @@ class Blog extends Component{
     state = {
         posts:[],
         selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
@@ -24,7 +25,11 @@ class Blog extends Component{
                 })
 
                 this.setState({posts: updatedPosts})
-            })   
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error: true});
+            })
     }
 
     postSelectedHandler = (id) => {
@@ -33,13 +38,16 @@ class Blog extends Component{
 
 
     render(){
-        const posts= this.state.posts.map(post => {
-            return <Post 
-            key={post.id} 
-            title={post.title} 
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts= <p>Something went wrong!</p>
+        if(!this.state.error) {
+            posts= this.state.posts.map(post => {
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
+            })
+        } 
 
         return(
             <div>
